6f43a6cb0dd840ee2722e75d698bc99af9a6062e
CheeseHackerXYZ <159737858+CheeseHackerXYZ@users.noreply.github.com>
2026-08-05 23:49:06 +0900

    Update user_ui.js

--- AI_generate/9/auction/user_ui.js
+++ AI_generate/9/auction/user_ui.js
@@ -54,3 +54,14 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 });
+
+
+function escapeHtml(str) {
+  if (!str) return '';
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+}
