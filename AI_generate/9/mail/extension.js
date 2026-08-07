846498f5beed7d5583ed448ee2a822f6d0ed97ee
CheeseHackerXYZ <159737858+CheeseHackerXYZ@users.noreply.github.com>
2026-08-07 23:09:17 +0900

    Update extension.js

--- AI_generate/9/mail/extension.js
+++ AI_generate/9/mail/extension.js
@@ -46,9 +46,8 @@ document.addEventListener('DOMContentLoaded', () => {
     sender: '依頼人',
     date: null,
     content: '箱の解除に成功し、呪縛は解けたようだ。更新が止まっていたブログも更新されて、体調も回復したらしい。君のおかげだ、ありがとう。<br><br><a href="../clear/index.html">GAME CLEAR</a>',
-    isRead: false
-  };
-  
+    isRead: 
+      
   /**
    * 日付を YYYY/MM/DD HH:mm 形式でフォーマット
    */
